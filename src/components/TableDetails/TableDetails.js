bec5b237e913f97ea9f8f7b481b8ce2c4c6ca3c1
Magdalena Sojka <sojka.magdalena.90@gmail.com>
2022-04-26 19:54:14 +0200

    Add changes in component TableDetails

--- src/components/TableDetails/TableDetails.js
+++ src/components/TableDetails/TableDetails.js
@@ -1,3 +1,27 @@
-const TableDetails = () => {}
+import { Form, Button } from 'react-bootstrap';
+
+const TableDetails = () => {
+
+  return (
+    <>
+      <h4 className="py-3">Table</h4>
+      <Form>
+        <Form.Group className="mb-3 d-flex justify-content-between align-items-center">
+          <Form.Label className="ms-2 me-3">Status</Form.Label>
+          <Form.Control type="form-select" />
+        </Form.Group>
+        <Form.Group className="mb-3 d-flex justify-content-between align-items-center">
+          <Form.Label  className="ms-2 me-3">People</Form.Label>
+          <Form.Control type="text" /> / <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3 d-flex justify-content-between align-items-center">
+          <Form.Label  className="ms-2 me-3">Bill</Form.Label>
+          $<Form.Control type="text" />
+        </Form.Group>
+        <Button variant="primary">Update</Button>
+      </Form>
+    </>
+  )  
+}
 
 export default TableDetails;
