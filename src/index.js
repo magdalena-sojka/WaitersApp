10037cdf5098fded38f144b7775d4993a619eb7c
Magdalena Sojka <sojka.magdalena.90@gmail.com>
2022-09-08 10:22:00 +0200

    Corrections

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-      </BrowserRouter>  
-  </React.StrictMode>,
-  document.querySelector('#root')
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 );
